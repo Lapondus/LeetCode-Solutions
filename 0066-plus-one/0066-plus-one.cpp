9ba3730e4ccd9a039e6dd37e247ed284ac4b3ae2
Mateusz Soszyński <63613368+Lapondus@users.noreply.github.com>
2023-08-11 13:54:33 +0200

    Time: 0 ms (100%), Space: 8.8 MB (29.46%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size();
+        for(int i = len-1; i>-1; i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }else{
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        digits[0]=1;
+        digits.push_back(0);
+        return digits;
+    }
+};
